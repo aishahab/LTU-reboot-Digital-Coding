e9108fd5cfc25bdcfbd785dd0d1ce4d79a5591ef
aishahab <72577547+aishahab@users.noreply.github.com>
2020-11-05 21:59:12 +0000

    Update task-3.1.6.js

--- scripts/task-3.1.6.js
+++ scripts/task-3.1.6.js
@@ -1,6 +1,18 @@
-var kittensName = 'Fluffy ';
-var fullName = kittensName + 'McDougle';
+var pieItem = 'Pie = 7' 
+var cakeItem = 'Cake = 5'
+var sconeItem = 'Scone = 3'
+var coffeeItem = 'Coffee = 2'
+var teaItem = 'Tea = 2'
 
-alert(fullName);
 
-document.write(fullName);
+var priceOfPie = 7;
+var priceOfCake = 5;
+var priceOfScone = 3;
+var priceOfCoffee = 2;
+var priceOfTea = 2;
+
+
+var billTotal = priceOfPie + priceOfCake + priceOfScone + priceOfCoffee + priceOfTea;
+
+document.write(billTotal);
+
