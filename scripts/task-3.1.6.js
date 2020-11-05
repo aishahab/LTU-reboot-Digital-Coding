36fe95d3edab108178b68e37e5bad5b014e240f5
aishahab <72577547+aishahab@users.noreply.github.com>
2020-11-05 22:36:12 +0000

    Update task-3.1.6.js

--- scripts/task-3.1.6.js
+++ scripts/task-3.1.6.js
@@ -1,9 +1,8 @@
-var pieItem = 'Pie = 7' 
-var cakeItem = 'Cake = 5'
-var sconeItem = 'Scone = 3'
-var coffeeItem = 'Coffee = 2'
-var teaItem = 'Tea = 2'
-
+var pieItem = 'Pie = &pound;7 ';
+var cakeItem = 'Cake = &pound;5 ';
+var sconeItem = 'Scone = &pound;3 ';
+var coffeeItem = 'Coffee = &pound;2 ';
+var teaItem = 'Tea = &pound;2 ';
 
 var priceOfPie = 7;
 var priceOfCake = 5;
@@ -11,8 +10,21 @@ var priceOfScone = 3;
 var priceOfCoffee = 2;
 var priceOfTea = 2;
 
+document.write(pieItem);
+document.write(cakeItem);
+document.write(sconeItem);
+document.write(coffeeItem);
+document.write(teaItem);
 
 var billTotal = priceOfPie + priceOfCake + priceOfScone + priceOfCoffee + priceOfTea;
 
-document.write(billTotal);
+var tipPercent = 20;
+var tipAmount = (billTotal / 100) * tipPercent;
 
+
+var totalPaid = billTotal + tipAmount;
+
+var message = 'Your bill was &pound;' + billTotal + ', you tipped ' + tipPercent + '%, which comes to &pound;' + tipAmount + ', taking your total paid to &pound;' + totalPaid;
+
+console.log(message);
+document.write(message);
