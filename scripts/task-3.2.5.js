4bd6ba944af57255242f78aa563478a1cb4e58c0
aishahab <72577547+aishahab@users.noreply.github.com>
2020-11-10 21:16:16 +0000

    made a variety of changes

--- scripts/task-3.2.5.js
+++ scripts/task-3.2.5.js
@@ -1,21 +1,14 @@
-var temperature = 40;
+var temperature = 90;
 
-if(temperature < 50){
-console.log('You need to wear a coat') //statement to excecute if it is true
-}
-
-if(temperature < 50){
-    console.log('You need to wear a coat') //statement to excecute if it is true
-    }
-
-    if(temperature < 30){
-        console.log('You need to wear a coat AND A HAT') //statement to excecute if it is true
-        }
-
-        if(temperature < 0){
-            console.log('You need to stay inside') //statement to excecute if it is true
-            }
-
-            else{
-                console.log('Rest easy in shorts and a vest')
+if(temperature < 0){
+    console.log('Stay inside') 
+    
+} else if (temperature < 30){
+        console.log('Wear a coat and a hat')
+    
+    } else if (temperature < 50){
+console.log('Wear a coat')
+    
+} else {
+            console.log('Rest easy in pants and a vest')
             }
