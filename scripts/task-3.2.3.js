876e6bf9b64d608840708e90b8e6826cb2dab209
aishahab <72577547+aishahab@users.noreply.github.com>
2020-11-10 20:18:29 +0000

    Update task-3.2.3.js

--- scripts/task-3.2.3.js
+++ scripts/task-3.2.3.js
@@ -1,6 +1,6 @@
 function fullName( firstName, lastName ) {
 
- return firstName + ' ' + lastName ;    
+ return firstName + ' ' + lastName ;  // return key stores the result for later use
 }
 
 var userName = fullName('Henry', 'Cavill');
