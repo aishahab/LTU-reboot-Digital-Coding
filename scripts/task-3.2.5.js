a9df0e96f2df492440c635a9f0b9f6e50c0183ca
aishahab <72577547+aishahab@users.noreply.github.com>
2020-11-10 20:54:42 +0000

    created task-3.2.5

---
+++ scripts/task-3.2.5.js
@@ -0,0 +1,21 @@
+var temperature = 40;
+
+if(temperature < 50){
+console.log('You need to wear a coat') //statement to excecute if it is true
+}
+
+if(temperature < 50){
+    console.log('You need to wear a coat') //statement to excecute if it is true
+    }
+
+    if(temperature < 30){
+        console.log('You need to wear a coat AND A HAT') //statement to excecute if it is true
+        }
+
+        if(temperature < 0){
+            console.log('You need to stay inside') //statement to excecute if it is true
+            }
+
+            else{
+                console.log('Rest easy in shorts and a vest')
+            }
