7477d45ecc386cb7c3a646e99fbeb66fb15d2565
aishahab <72577547+aishahab@users.noreply.github.com>
2020-11-10 20:44:29 +0000

    Update task-3.2.4.js

--- scripts/task-3.2.4.js
+++ scripts/task-3.2.4.js
@@ -1,5 +1,5 @@
-var temperature = 30;
+var temperature = 40;
 
 if(temperature < 50){
-console.log('You need to wear a coat')
+console.log('You need to wear a coat') //statement to excecute if it is true
 }
