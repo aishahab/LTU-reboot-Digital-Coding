1de04f87a520b146eac368abaa74aa521f440e15
aishahab <72577547+aishahab@users.noreply.github.com>
2020-11-10 20:10:58 +0000

    created tasks

---
+++ scripts/task-3.2.3.js
@@ -0,0 +1,10 @@
+function fullName (firstName, lastName) {
+
+ console.log (firstName + ' ' + lastName);    
+
+}
+
+fullName ('Henry', 'Cavill');
+fullName ('Aishah', 'Bashir');
+fullName ('Sam', 'Johnson');
+fullName ('Allie', 'James');
