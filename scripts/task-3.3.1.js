7039a7093236eafaf880a6bfec9356917fc4f3e9
aishahab <72577547+aishahab@users.noreply.github.com>
2020-11-12 19:31:09 +0000

    Create task-3.3.1.js

---
+++ scripts/task-3.3.1.js
@@ -0,0 +1,22 @@
+var temperature = 90;
+
+if(temperature < 0){
+    console.log('Stay inside') 
+    
+} else if (temperature < 30){
+        console.log('Wear a coat and a hat')
+    
+    } else if (temperature < 50){
+console.log('Wear a coat')
+    
+} else if ( temperature > 50 && temperature === 55) {
+console.log ('Its very hot')
+
+} else if (temperature > 50 || temperature === 55) {
+console.log ('Its extremely hot, looks like someone is going to get  burnt')
+    
+} else {
+console.log('Rest easy in pants and a vest')
+            }
+
+// logical operators && (AND) || (OR) ! (NOT)
