e038607e374b38353704995f7cb7cc311c2c5fa9
aishahab <72577547+aishahab@users.noreply.github.com>
2020-11-12 19:56:15 +0000

    Create task-3.3.2.js

---
+++ scripts/task-3.3.2.js
@@ -0,0 +1,16 @@
+function percentageCalculator(number, percentage) {
+
+var percentageOf = number * percentage / 100;
+//BODMAS (BRACKETS ORDER DIVISION MULTIPLICATION ADDITION SUBTRACTION)
+
+return percentageOf;
+
+
+}
+
+console.log(percentageCalculator(300, 25));
+
+
+var twentyFivePercentOfHundred = percentageCalculator(100, 25);
+
+console.log(twentyFivePercentOfHundred);
