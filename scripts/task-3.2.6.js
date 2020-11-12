d15625296fdac7a6aa56b4be68b399007dc3808e
aishahab <72577547+aishahab@users.noreply.github.com>
2020-11-12 19:29:58 +0000

    Update task-3.2.6.js

--- scripts/task-3.2.6.js
+++ scripts/task-3.2.6.js
@@ -12,11 +12,11 @@ console.log('Wear a coat')
 } else if ( temperature > 50 && temperature === 55) {
 console.log ('Its very hot')
 
-} else if (temperature > 50 && temperature === 55) {
+} else if (temperature > 50 || temperature === 55) {
 console.log ('Its extremely hot, looks like someone is going to get  burnt')
     
 } else {
 console.log('Rest easy in pants and a vest')
             }
 
-// logical operators && (AND) !! (OR) ! (NOT)
+// logical operators && (AND) || (OR) ! (NOT)
