89bbbfe1130d0d1f375086fffde575a5c3aa95f9
aishahab <72577547+aishahab@users.noreply.github.com>
2020-11-12 19:17:22 +0000

    created tasks

---
+++ scripts/task-3.2.6.js
@@ -0,0 +1,14 @@
+var temperature = 90;
+
+if(temperature < 0){
+    console.log('Stay inside') 
+    
+} else if (temperature < 30){
+        console.log('Wear a coat and a hat')
+    
+    } else if (temperature < 50){
+console.log('Wear a coat')
+    
+} else {
+            console.log('Rest easy in pants and a vest')
+            }
