a38b31027cc0a703865614a9bc83ebd0bc2ebf50
aishahab <72577547+aishahab@users.noreply.github.com>
2020-11-17 20:56:13 +0000

    Update task-3.4.3.js

--- scripts/task-3.4.3.js
+++ scripts/task-3.4.3.js
@@ -1,7 +1,9 @@
-var favouriteFoods = ['Pizza', 'Ice Cream', 'Kebab', 'Chips'];
+var favouriteFoods = ['Pizza', ' Ice Cream', ' Kebab', ' Chips'];
 
 for(var index = 0; index < favouriteFoods.length; index++) {
 
-    document.write ( favouriteFoods[index]);
+    console.log(favouriteFoods[1]); //ice cream
+    console.log(favouriteFoods[3]); //chips
 
 }
+
