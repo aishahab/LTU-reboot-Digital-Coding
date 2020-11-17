ec145ac769e49377d831314441d6946603740c53
aishahab <72577547+aishahab@users.noreply.github.com>
2020-11-17 21:32:54 +0000

    Create task-3.4.4.js

---
+++ scripts/task-3.4.4.js
@@ -0,0 +1,9 @@
+var favouriteFoods = ['Pizza', ' Ice Cream', ' Kebab', ' Chips'];
+
+for(var index = 0; index < favouriteFoods.length; index++) {
+
+    console.log(favouriteFoods[1]); //ice cream
+    console.log(favouriteFoods[3]); //chips
+
+}
+
