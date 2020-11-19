3896c212e254510da6d4e66fd510525f2c8bb260
aishahab <72577547+aishahab@users.noreply.github.com>
2020-11-19 19:24:26 +0000

    Update task-3.4.4.js

--- scripts/task-3.4.4.js
+++ scripts/task-3.4.4.js
@@ -1,9 +1,37 @@
-var favouriteFoods = ['Pizza', ' Ice Cream', ' Kebab', ' Chips'];
+//Create an object to hold information on your favorite recipe. Then display the properties on screen.​
+//Bonus Points: Create a loop to list all the ingredients.​
 
-for(var index = 0; index < favouriteFoods.length; index++) {
+//Recipe properties:​
+//recipeTitle (a string)​
+//servings (a number)​
+//ingredients (an array of strings)​
+//directions (a string)​
 
-    console.log(favouriteFoods[1]); //ice cream
-    console.log(favouriteFoods[3]); //chips
+var favouriteRecipe = {
+recipeTitle: 'Spaghetti Bolognese',
+servings: 6,
+ingredients: [
+'1 tbsp olive oil',
+'500g beef mince',
+'tomato sauce',
+'spaghetti',
+],
+directions: ['Make Bolognese sauce', 'Step 2', 'Step 3'],
+letsCook: function(){
 
+    console.log('Im hungry, lets cook' + favouriteRecipe, recipeTitle);
+}
+};
+
+
+//bonus part of task
+var ingredients = favouriteRecipe.ingredients
+
+for(var index = 0; index < ingredients.length; index){
+
+    //console.log(ingredients[index])
 }
 
+//console.log(Object.keys(favouriteRecipe));
+
+console.log(Object.entries(favouriteRecipe));
